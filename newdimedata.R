ad54d91b24f7aad78693f5571ccf74f8dc3eff18
heewon-yoon <hy2571@columbia.edu>
2024-09-04 15:08:06 -0400

    09/04/2024

--- newdimedata.R
+++ newdimedata.R
@@ -19,7 +19,7 @@ library(reshape)
 #-------------------------------------------------------
 # EM-DAT
 #-------------------------------------------------------
-emdat <- read_excel("/Users/hyoon/Desktop/Yoon2/substitution/sub/public_emdat_custom_request_2024-08-22_d4735aca-f11f-483c-b698-b41673693d0e.xlsx") %>% 
+emdat <- read_excel("/Users/hyoon/Desktop/Yoon2/substitution/sub/public_emdat_custom_request_2024-08-23_a6cf34e3-50c6-4a7e-82c2-6ac4c7f3abdb.xlsx") %>% 
   filter(Country == "United States of America") %>%
   select("DisNo.", "Disaster Subgroup", "Disaster Type", "Disaster Subtype", "Event Name", "Location", 
          "Start Year", "Start Month", "Start Day", "Total Deaths", "No. Injured", "No. Affected", "Total Affected",
@@ -44,11 +44,10 @@ extract_states <- function(elem) {
   return(extracted_states)
 }
 
-emdat$Location <- sapply(emdat$Location, extract_states)
-
-# expand to state level
-
-emdat <- emdat %>% mutate(Location = strsplit(as.character(Location), ",")) %>% unnest(Location)
+emdat <- emdat %>% mutate(Location = sapply(emdat$Location, extract_states)) %>%
+  mutate(Location = strsplit(as.character(Location), ",")) %>% unnest(Location) %>%
+  mutate(Location = trimws(.$Location)) %>%
+  mutate(state = state.abb[match(.$Location, state.name)]) %>% distinct()
 #-------------------------------------------------------
 
 
@@ -59,10 +58,73 @@ fema <- read.csv("/Users/hyoon/Desktop/Yoon2/substitution/sub/FemaWebDeclaration
   mutate(designatedDate = as.Date(substr(.$designatedDate, 1, 10)),
          countycode = as.numeric(str_sub(.$placeCode, 3)),
          year = as.integer(substr(.$designatedDate, start=1, stop=4)),
-         month = as.integer(substr(.$designatedDate, start=6, stop=7))) %>%
+         month = as.integer(substr(.$designatedDate, start=6, stop=7)),
+         day = as.integer(substr(.$designatedDate, start=9, stop=10))) %>%
   mutate(countycode = sprintf("%03d", as.numeric(.$countycode))) %>% 
-  select("disasterNumber", "programTypeDescription", "stateCode", "placeName", "designatedDate", "countycode", "year", "month") %>%
-  filter(designatedDate >= "2000-01-01" & designatedDate < "2022-12-12")
+  filter(designatedDate >= "2000-01-01" & designatedDate < "2022-12-12") %>%
+  select("disasterNumber", "programTypeDescription", "stateCode", "placeName", "designatedDate", "countycode", "year", "month", "day")
+#-------------------------------------------------------
+
+
+#-------------------------------------------------------
+# Merge FEMA and EMDAT
+#-------------------------------------------------------
+# fema is county level
+# emdat is state level, has disaster magnitude
+
+disaster <- left_join(fema, emdat, 
+                          by = c("stateCode"="state",
+                                 "year"="Start Year",
+                                 "month"="Start Month")) %>% 
+  drop_na("DisNo.") 
+
+# create variable that is the distance b/w the days
+#  %>% mutate(diff = abs(.$"day" - .$"Start Day"))
+# group by county, disaster
+# keep one that minimizes that variable
+# d <- disaster %>% group_by(stateCode, countycode, DisNo., year, month) %>% slice(which.min(diff))
+
+# multiple disasters in state, county, year, month
+  
+disaster_day <- left_join(fema, emdat, 
+                      by = c("stateCode"="state",
+                             "year"="Start Year",
+                             "month"="Start Month",
+                             "day"="Start Day")) %>% drop_na("DisNo.") 
+# rows with no DisNo means no match (doesn't exist in EM-DAT)
+
+# there are cases with multiple disasters within a specific month+year for a state-county
+# but needs to be aggregated to district level anyways so leaving as is for now
+#-------------------------------------------------------
+
+
+#-------------------------------------------------------
+# Map counties to districts
+#-------------------------------------------------------
+# refer to disaster.rmd / postapsa.rmd
+# used census data that maps county to districts
+# merged with primary election dates 
+cong <- read.csv("/Users/hyoon/Desktop/Yoon2/11. 2023Fall/apsa_interdistrict/apsadonation/cong.csv") %>% select(-X)
+#-------------------------------------------------------
+
+
+#-------------------------------------------------------
+# Aggregate county level disaster data to state level
+#-------------------------------------------------------
+# disaster is county level
+# cong is district level
+# note there are multiple disasters within county
+
+trial <- left_join(cong %>% mutate(county = as.numeric(.$county),
+                                   anl.begin = as.Date(.$anl.begin),
+                                   pri = as.Date(.$pri)), 
+                   disaster %>% mutate(countycode = as.numeric(.$countycode)),
+                   by = c("county"="countycode",
+                          "abb"="stateCode")) %>% 
+  filter(anl.begin <= designatedDate & pri >= designatedDate) %>%
+  mutate(close = .$pri - .$designatedDate) %>%
+  distinct(county, abb, year.x, DisNo., .keep_all= TRUE)
+
 #-------------------------------------------------------
 
 
@@ -104,3 +166,5 @@ fema <- read.csv("/Users/hyoon/Desktop/Yoon2/substitution/sub/FemaWebDeclaration
 cand <- read.csv("/Users/hyoon/Desktop/Yoon2/substitution/dime 08:21:24/cand.csv") %>% select(-X)
 cont <- read.csv("/Users/hyoon/Desktop/Yoon2/substitution/dime 08:21:24/cont.csv") %>% select(-c(X, X.1))
 #-------------------------------------------------------
+
+
